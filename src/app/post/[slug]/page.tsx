232f99fee5bc6c261349b005e8f1e1e8d6ed12a6
Vamsi Krishna Kodimela <vamsikrishnakdml@gmail.com>
2025-07-17 13:36:28 +0530

    Enhance PostPage component layout with CSS styles and update post content with HTML structure for better rendering

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,5 +1,5 @@
-import { Post } from "@/components/post";
 import { POSTS } from "@/data/posts";
+import styles from "./page.module.css";
 
 interface PostPageProps {
   params: {
@@ -13,7 +13,27 @@ const PostPage = async ({ params }: PostPageProps) => {
   if (!post) {
     return <div>Post not found</div>;
   }
-  return <Post post={post} />;
+  return (
+    <div className={styles.layout}>
+      <img
+        src={post.featuredImage}
+        alt={post.title}
+        className={styles.featuredImage}
+      />
+      <div className={styles.metadata}>
+        <div className={styles.category}>{post.category}</div>
+        <div className={styles.createdAt}>
+          {post.createdAt.toLocaleDateString()}
+        </div>
+      </div>
+      <h2 className={styles.title}>{post.title}</h2>
+      <p className={styles.description}>{post.description}</p>
+      <div
+        className={styles.content}
+        dangerouslySetInnerHTML={{ __html: post.content }}
+      />
+    </div>
+  );
 };
 
 export default PostPage;
