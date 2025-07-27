0ddf1a1df0ae2e1d60f0520be8c21e0080761a9d
Vamsi Krishna Kodimela <vamsikrishnakdml@gmail.com>
2025-07-27 18:21:26 +0530

    Refactor styles for CategorySection, Post, and PostsSection components: transition shimmer loading states to flexbox for improved layout consistency, enhance responsiveness with updated aspect ratios and min-height properties, and optimize layout calculations to prevent shifts during loading. Update typography and spacing for better readability across devices.

--- src/components/post/index.tsx
+++ src/components/post/index.tsx
@@ -50,6 +50,8 @@ const Post = ({ post }: IPostProps) => {
           height={200}
           priority={false}
           loading="lazy"
+          placeholder="blur"
+          blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWGRkqGx0f/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="
         />
       </div>
 
